1d7e8f0025a1129c9bd5745d78a56f09fd2498f0
znarfm <44516102+znarfm@users.noreply.github.com>
2025-11-19 16:36:26 +0800

    chore: remove webpack dependency

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,7 @@
 import type { NextConfig } from 'next'
- 
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  
-  // This block is for Turbopack (next dev)
-  // It's a top-level property, not inside 'experimental'
-  turbopack: {
-    rules: {
-      // Add this rule for SVGs
-      '*.svg': {
-        as: '*.js', // Treat SVG imports as JavaScript modules
-        loaders: ['@svgr/webpack'],
-      },
-    },
-  },
 
-  // This block is for Webpack (next build)
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
- 
-    return config
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 }
- 
+
 export default nextConfig
