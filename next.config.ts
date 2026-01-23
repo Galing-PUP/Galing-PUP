ab21c0d30dd94717a4735c1366fc15f48bea7881
yam-1111 <achinay0341ant@student.fatima.edu.ph>
2026-01-24 00:20:48 +0800

    test4

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
+const nextConfig: any = {
   /* config options here */
   serverExternalPackages: ['pdfjs-dist'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/admin/ingest': ['./node_modules/pdfjs-dist/build/**/*'],
+    },
+  },
   compiler: {
     removeConsole: {
       exclude: ['error', 'warn'],
