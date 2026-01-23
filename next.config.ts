fca1b1df214da988b9752091c4fd4436935d468c
yam-1111 <achinay0341ant@student.fatima.edu.ph>
2026-01-24 00:02:02 +0800

    test: remove webpack

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ['pdfjs-dist'],
-  webpack: (config) => {
-    config.resolve.alias.canvas = false
-    return config
-  },
   compiler: {
     removeConsole: {
       exclude: ['error', 'warn'],
